061c6a384cead190a145f35b2bf7a78c4a3e25ce
Rasmus Joel Raidjõe <rasmusjoel.raidj6e@gmail.com>
2025-01-27 19:37:12 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="#">Contact--</a></li>
         </ul>
       </nav>
       <main>
