5fad5609b6bdb7efc5e6937d25d4dacca7afbd6a
Rasmus Joel Raidjõe <rasmusjoel.raidj6e@gmail.com>
2025-01-27 19:29:20 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count1, setCount1] = useState(0)
-  const [count2, setCount2] = useState(0)
+  const [fileContent, setFileContent] = useState('');
+
+
+  /*useEffect(() => {
+    fetch('/esileheKirjeldus.txt')
+      .then((response) => response.text())
+      .then((data) => setFileContent(data));
+  }, []);*/
 
   return (
     <>
-      <div>
+    <body>
+      <header>
 
-      </div>
-      <h1>Eesti Liputoimkondade Liit</h1>
-      <h2>Hello</h2>
+      </header>
 
-      <div>
-        
-        {' '}<button onClick={() => setCount1((count) => count1 - 1)}>
-            -
-        </button>
-        {' '}<button onClick={() => setCount1((count) => count1 + 1)}>
-            +
-        </button>
-
-        &nbsp;&nbsp; a: &nbsp; {count1}
-      </div>
-
-      <div>
-        
-        {' '}<button onClick={() => setCount2((count2) => count2 - 1)}>
-            -
-        </button>
-        {' '}<button onClick={() => setCount2((count2) => count2 + 1)}>
-            +
-        </button>
-
-        &nbsp;&nbsp; b: &nbsp; {count2}
-      </div>
-
-      <div className="card">
-        {count1}{' '} + {' '}{count2}
-        {' '}={' '}
-        {count1 + count2}
-
-
-      </div>
-      <p className="read-the-docs">
-        tere 2.0
-      </p>
+      <nav>
+        <ul class = "your-class">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Liputoimkond</h1>
+        <div className = "text">
+          <p>
+          {fileContent}
+          </p>
+        </div>
+      </main>
+      <section></section>
+      <article></article>
+      <aside></aside>
+      <footer></footer>
+    </body>
     </>
   )
 }
