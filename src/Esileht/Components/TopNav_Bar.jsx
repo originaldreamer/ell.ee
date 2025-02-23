71cacb6a75b6b9c000a639cba60edcde3af6a89b
DELEMV <mattisraabis@gmail.com>
2025-02-23 16:47:37 +0000

    mingi kiri

--- src/Esileht/Components/TopNav_Bar.jsx
+++ src/Esileht/Components/TopNav_Bar.jsx
@@ -46,12 +46,22 @@ function TopNavigationBarButtons() {
             </div>
 
             <div className="dropdown">
-            <button className="dropbtn">Sümboolika ▼</button>
-              <div className="dropdown-content">
-                <div><a href="#">Vappide Kirjeldus</a></div>
-                <div><a href="#">Lugu</a></div>
-                <div><a href="#">Kasutus</a></div>
-              </div>
+            <button className="dropbtn">
+              Sümboolika
+              <svg
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="white"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              style={{ marginLeft: "5px", transform: "translateY(3px)" }}
+              >
+              <polyline points="6 9 12 15 18 9"></polyline>
+              </svg>
+            </button>
             </div>
 
             <div className="dropdown">
