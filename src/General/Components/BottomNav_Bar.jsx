963e5a83859f456931a196f1f3eeec611cb3e451
Rasmus Joel Raidjõe <rasmusjoel.raidj6e@gmail.com>
2025-02-24 12:12:15 +0200

    fixed a small bug hopefully naming

--- src/General/Components/BottomNav_Bar.jsx
+++ src/General/Components/BottomNav_Bar.jsx
@@ -4,12 +4,12 @@ import './BottomNav_Bar.css'
 
 
 //imported images
-import detailneLogo from '/src/General/Design elements/Vapp detailne.png'
-import kontaktideTaust from '/src/General/Design elements/Kontaktide taust lipukujuline.svg'
-import gmailLogo from '/src/General/Design elements/gmail logo.svg'
-import instagramLogo from '/src/General/Design elements/instagram logo.svg'
-import gmailLogoClicked from '/src/General/Design elements/gmail logo clicked.svg'
-import instagramLogoClicked from '/src/General/Design elements/instagram logo clicked.svg'
+import detailneLogo from '../Design elements/Vapp detailne.png'
+import kontaktideTaust from '../Design elements/Kontaktide taust lipukujuline.svg'
+import gmailLogo from '../Design elements/gmail logo.svg'
+import instagramLogo from '../Design elements/instagram logo.svg'
+import gmailLogoClicked from '../Design elements/gmail logo clicked.svg'
+import instagramLogoClicked from '../Design elements/instagram logo clicked.svg'
 
 
 function Pilt( {pilt,kirjeldus, size}) {
