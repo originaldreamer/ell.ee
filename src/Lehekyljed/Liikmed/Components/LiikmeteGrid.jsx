46c0093e7419dbd2a53195c7ff35545128597562
Rasmus Joel Raidjõe <rasmusjoel.raidj6e@gmail.com>
2025-04-19 10:23:05 +0300

    made some imporvemetns to liikmed page

--- src/Lehekyljed/Liikmed/Components/LiikmeteGrid.jsx
+++ src/Lehekyljed/Liikmed/Components/LiikmeteGrid.jsx
@@ -10,23 +10,26 @@ import koolimajaPilt from '../Design Elements/MURG liputoimkond.jpg'
 
 import LiikmeTile from './LiikmeTile'
 
-
+ 
 
 export default function LiikmeteGrid({functionShowKirjeldus, loadContentFunction}) 
 {
     return (
-        <div className='liikmeteGrid'>
-            {[...Array(data.length)].map((_, index) => (
-                <LiikmeTile 
-                    key={index}  // Ensure each component has a unique key
-                    pilt={koolimajaPilt} 
-                    index={index}  // Pass the increasing index value
-                    handleClick={() => { 
-                        functionShowKirjeldus(); 
-                        loadContentFunction(index);  // Pass the corresponding index
-                    }}
-                />
-            ))} 
+        <div className='liikmeteGrid-container'>
+            <div className='liikmeteGrid-elemendid'>
+                {[...Array(data.length)].map((_, index) => (
+                    <LiikmeTile 
+                        key={index}  // Ensure each component has a unique key
+                        pilt={koolimajaPilt} 
+                        index={index}  // Pass the increasing index value
+                        handleClick={() => { 
+                            functionShowKirjeldus(); 
+                            loadContentFunction(index);  // Pass the corresponding index
+                        }}
+                    />
+                ))} 
+            </div>
+    
         </div>
         
     );
