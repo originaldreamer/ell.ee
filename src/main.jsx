bcb25ae8cdff503c0e795226f657c09d34346f68
Rasmus Joel Raidjõe <rasmusjoel.raidj6e@gmail.com>
2025-05-23 07:15:30 +0300

    pls work on samsung internet

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictModem } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,4 +7,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+) 
