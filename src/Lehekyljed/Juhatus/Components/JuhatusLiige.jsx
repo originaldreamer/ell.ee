c7b0c98adfe5b1b332d43f065eef87ce97319b47
Rasmus Joel Raidjõe <rasmusjoel.raidj6e@gmail.com>
2025-06-01 15:52:04 +0300

    juhatus korras

--- src/Lehekyljed/Juhatus/Components/JuhatusLiige.jsx
+++ src/Lehekyljed/Juhatus/Components/JuhatusLiige.jsx
@@ -12,7 +12,7 @@ export default function JuhatusLiige({pilt, nimi, kool, roll, meil, telefon}) {
             </div>
 
             <div className='juhatusLiige-bottom'>
-                <div className='mid-small-top-header' style={{marginTop: '12px'}}> {nimi} </div>
+                <div className='mid-small-top-header juhatusLiige-nimi' style={{marginTop: '12px'}}> {nimi} </div>
 
                 <div className='juhatusLiige-bottom-tekst'>
                     <div className='normal-text-white juhatusLiige-profile-text'>{roll}</div>
@@ -22,7 +22,7 @@ export default function JuhatusLiige({pilt, nimi, kool, roll, meil, telefon}) {
                     </div>
                     <div className='juhatusLiige-profile-kontakt-text'>
                         <LinkUnderlined display={"telefon: " + telefon} normalColor='#F5F5F5'/>
-                    </div>
+                    </div> 
 
                     
                 </div>
